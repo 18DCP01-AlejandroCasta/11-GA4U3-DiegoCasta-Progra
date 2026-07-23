fd0564550231d89b7ad92f5b768dd3d0adede4ea
Ale <d.castaneda.5cdb@gmail.com>
2026-07-22 18:56:18 -0600

    Add files via upload

---
+++ lecc34act1inc2.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("     CÁLCULO DEL IMC (PESO/ALTURA²)");
+
+        Console.Write("Ingrese su peso en kilogramos (ej. 70.5): ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese su altura en metros (ej. 1.75): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double imc = peso / Math.Pow(altura, 2);
+
+        Console.WriteLine("\n---------------------------------------");
+        Console.WriteLine($"Su IMC es: {imc:F2}");
+
+        if (imc < 18.5)
+        {
+            Console.WriteLine("Estado: Bajo peso");
+        }
+        else if (imc >= 18.5 && imc < 25)
+        {
+            Console.WriteLine("Estado: Peso normal");
+        }
+        else if (imc >= 25 && imc < 30)
+        {
+            Console.WriteLine("Estado: Sobrepeso");
+        }
+        else
+        {
+            Console.WriteLine("Estado: Obesidad");
+        }
+        Console.WriteLine("---------------------------------------");
+    }
+}
