62823a13212142e3ae4fd94e49100784ce25385c
Ale <d.castaneda.5cdb@gmail.com>
2026-07-22 18:53:42 -0600

    Add files via upload

---
+++ lecc33act2pag119.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese una frase: ");
+        string frase = Console.ReadLine();
+
+        string fraseInvertida = "";
+
+        for (int i = frase.Length - 1; i >= 0; i--)
+        {
+            fraseInvertida += frase[i];
+        }
+
+        Console.WriteLine("\n-------------------------------------------");
+        Console.WriteLine($"Frase original:  {frase}");
+        Console.WriteLine($"Frase invertida: {fraseInvertida}");
+        Console.WriteLine("-------------------------------------------");
+    }
+}
